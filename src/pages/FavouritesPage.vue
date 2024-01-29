1124c4ea2fca0e23cbe3d6d50378e51bbf1d9ab6
lakient <modenoff.nik@gmail.com>
2024-01-29 19:19:43 +0500

    initial commit

---
+++ src/pages/FavouritesPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <h3> Страница со списком любимых </h3>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'FavouritesPage'
+})
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <h3> Страница со списком тестов </h3>
+      <div class="q-pa-md" style="max-width: 350px">
+        <q-list bordered separator>
+          <q-item v-for="n in 10" :key="n" clickable v-ripple>
+            <q-item-section>Вопрос номер: {{n}}</q-item-section>
+            <q-item-section>Тема вопроса: {{n}}</q-item-section>
+            <q-item-section>Уровень подготовки: А{{n}}</q-item-section>
+            <q-item-section>Выполнен ли: А{{n}}</q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+    </div>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
